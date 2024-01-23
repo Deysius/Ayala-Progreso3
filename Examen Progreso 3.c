6d6ef817064f6105d385d292b117328362264861
Josue Ayala <josue.ayala.flores@udla.edu.ec>
2024-01-23 15:23:42 -0500

    Creo una estructura

---
+++ Examen Progreso 3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+struct alumno
+{
+    int matricula;
+    char nombre[50];
+    char direccion[50];
+    char materia[50];
+    float nota;
+    //Mediante una estructura designo los valores para cada uno de las variables
+};
