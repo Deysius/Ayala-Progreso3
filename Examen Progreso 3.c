17d7cea1348dd81e9a541fcab2ed8aab53007326
Josue Ayala <josue.ayala.flores@udla.edu.ec>
2024-01-23 15:26:30 -0500

    Pido al usuario los datos del estudiante

--- Examen Progreso 3.c
+++ Examen Progreso 3.c
@@ -19,7 +19,47 @@ int main()
     while (menu != 3)
     //Mediante un bucle while, hago que mi menu se repita infinitamente hasta que el usuario quiera salir
     {
-       
+        printf("Bienvenido al menú\n");
+        printf("(1) Agregar estudiantes\n");
+        printf("(2) Ver lista de estudiantes\n");
+        printf("(3) Salir\n");
+        scanf("%d", &menu);
+        switch (menu)
+        {
+        case 1:
+            printf("Ingrese la matrícula: ");
+            scanf("%d", &estudiante[num_estudiantes].matricula);
+            // Pido al usuario que añada los datos del estudiante
+            fprintf(archivo,"Matricula: %d\n",estudiante[num_estudiantes].matricula);
+            fflush(stdin);
+            printf("Ingrese el nombre: ");
+            fgets(estudiante[num_estudiantes].nombre, 50, stdin);
+            fprintf(archivo,"Nombre: %s\n",estudiante[num_estudiantes].nombre);
+            //utilizo la funcion fgets, para pedir que se añada caracteres, funcionan como un scanf
+            fflush(stdin);
+            // con la funcion fflush limpio la memoria, evitando errores 
+
+            printf("Ingrese la dirección: ");
+            fgets(estudiante[num_estudiantes].direccion, 50, stdin);
+            fprintf(archivo,"Direccion: %s\n",estudiante[num_estudiantes].direccion);
+            fflush(stdin);
+            // utilizo la funcion fgets, ya que el código no compila función gets() porque  está obsoleta en C99 y C11
+
+            printf("Ingrese la materia: ");
+            fgets(estudiante[num_estudiantes].materia, 50, stdin);
+            fprintf(archivo,"Materia: %s\n",estudiante[num_estudiantes].materia);
+            fflush(stdin);
+
+            printf("Ingrese la nota: ");
+            scanf("%f", &estudiante[num_estudiantes].nota);
+            fprintf(archivo,"Nota: %f\n",estudiante[num_estudiantes].nota);
+            fflush(stdin);
+
+            num_estudiantes++;
+            fflush(stdin);
+            fclose(archivo);
+            break;
+
         case 2:
         break;
         default:
