687fd22ad5a7d76a27327e186defe4952ff676d7
Josue Ayala <josue.ayala.flores@udla.edu.ec>
2024-01-23 15:25:12 -0500

    Incluyo un menu y el archivo

--- Examen Progreso 3.c
+++ Examen Progreso 3.c
@@ -9,3 +9,22 @@ struct alumno
     float nota;
     //Mediante una estructura designo los valores para cada uno de las variables
 };
+int main()
+{
+    int menu;
+    struct alumno estudiante[50];
+    int num_estudiantes = 0;
+    FILE*archivo;
+    archivo=fopen("Examen.txt","a");
+    while (menu != 3)
+    //Mediante un bucle while, hago que mi menu se repita infinitamente hasta que el usuario quiera salir
+    {
+       
+        case 2:
+        break;
+        default:
+            break;
+        }
+    }
+    return 0;
+}
